212fcc77ef6248b66d7281a04deda0d7a2658a96
Aditya Mathur <adityamathur5683@gmail.com>
2025-12-07 23:37:44 +0530

    feat: add responsive navbar with routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import NavBar from "./components/NavBar";
 
+function App() {
   return (
-    <>
-    <div className="bg-amber-500" >
-      this is nothing 
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+      {/* Background gradient*/}
+      <div className="absolute inset-0 overflow-hidden z-0">
+        <div className="absolute inset-0">
+          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]" />
+        </div>
+      </div>
+      
+      <div className="relative z-50">
+        <NavBar />
+      </div>
+      
+      {/* Content */}
+      <div className='relative z-40 pt-20'>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
